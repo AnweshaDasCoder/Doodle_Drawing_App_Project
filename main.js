899b0fe3e49a6423266855dd3cf43b2a4569f863
AnweshaDasCoder <95891366+AnweshaDasCoder@users.noreply.github.com>
2022-08-24 00:29:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+var timer_counter = 0;
+
+var timer_check = "";
+
+var drawn_sketch = "";
+
+var answer_holder = "";
+
+var score = 0;
+
+ array_1=["pen","paper","book","bottle"]
+random_no = Math.floor((Math.random()*array_1.length)+1)
+Element_of_array = array_1[random_no]
+
+console.log("random_no" + random_no );
+
+array_1 = random_no;
+
